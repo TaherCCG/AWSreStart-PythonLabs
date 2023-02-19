a06afe4620891084d2654b3f5770db1c7fd91a88
TaherCCG <Taher.ccg@gmail.com>
2023-02-19 23:42:22 +0000

    upload net-charge.py

---
+++ L12-netCharge/net-charge.py
@@ -0,0 +1,26 @@
+# Python3.6  
+# Coding: utf-8  
+# Store the human preproinsulin sequence in a variable called preproinsulin:  
+preproInsulin = "malwmrllpllallalwgpdpaaafvnqhlcgshlvealylvcgergffytpktrreaedlqvgqvelgggpgagslqplalegslqkrgiveqcctsicslyqlenycn"  
+# Store the remaining sequence elements of human insulin in variables:  
+lsInsulin = "malwmrllpllallalwgpdpaaa"  
+bInsulin = "fvnqhlcgshlvealylvcgergffytpkt"  
+aInsulin = "giveqcctsicslyqlenycn"  
+cInsulin = "rreaedlqvgqvelgggpgagslqplalegslqkr"  
+insulin = bInsulin + aInsulin 
+pKR = {'y': 10.07,'c': 8.18,'k': 10.53,'h': 6.00,'r': 12.48,'d': 3.65,'e': 4.25}
+
+insulin.count("Y")
+float(insulin.count("Y"))
+seqCount = ({x: float(insulin.count(x)) for x in ['y','c','k','h','r','d','e']})
+print(seqCount)    # added for testing to see dictionary key and values
+pH = 0
+while (pH <= 14):
+    netCharge = (
+        +(sum({x: ((seqCount[x]*(10**pKR[x]))/((10**pH)+(10**pKR[x]))) \
+        for x in ['k','h','r']}.values()))
+        -(sum({x: ((seqCount[x]*(10**pH))/((10**pH)+(10**pKR[x]))) \
+        for x in ['y','c','d','e']}.values())))
+    print('{0:.2f}'.format(pH), netCharge)
+    pH +=1
+    
