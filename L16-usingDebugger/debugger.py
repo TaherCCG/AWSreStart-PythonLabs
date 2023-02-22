c09129ca5c1b0cd16052c08bb6504c8b3399bd67
TaherCCG <taher.ccg@gmail.com>
2023-02-22 17:33:48 +0000

    added folder L16-usingDebugger and debugger.py

---
+++ L16-usingDebugger/debugger.py
@@ -0,0 +1,21 @@
+msg= """
+Lab overview
+
+A software bug refers to an error, flaw, or failure in a computer program that causes an incorrect 
+or unexpected result. A debugger is a computer program that is used to test and find bugs (debug) 
+other programs. You can use a debugger to step through the code. The Python Debugger (pdb) is an 
+interactive source code debugger for Python programs. In this lab, you will use the pdb to step 
+through the scripts you wrote in previous labs.
+
+In this lab, you will:
+
+-  Explore the basic features of the Python Debugger
+-  Use the Python Debugger to step through Python scripts
+"""
+print(msg)
+print("This program was provided in this lab to show how to use debugger on cloud9\n\n")
+
+name = "John"
+print("Hello " + name + ".")
+age = 40
+print(name + " is " + str(age) + " years old.")
